83e4172510b3b73b20a4704a959842eeccde3178
Rizal Anggoro <gnoogler4@gmail.com>
2025-07-23 08:55:17 +0700

    fix: load env

--- main.go
+++ main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"api/pkg/http"
+	"fmt"
 	"github.com/joho/godotenv"
-	"log"
 )
 
 // @title		PresensiGo API
 // @version	1.0
 func main() {
 	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
+		fmt.Println("error loading .env file")
 	}
 
 	http.NewServer()
