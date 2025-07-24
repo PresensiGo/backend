47d3abe19d90263a6e66a6dc787f65790ed85d40
Rizal Anggoro <gnoogler4@gmail.com>
2025-07-25 06:22:17 +0700

    fix: dependency injection for database

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -6,43 +6,55 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"os"
+	"sync"
+)
+
+var (
+	dbInstance *gorm.DB
+	dbOnce     sync.Once
 )
 
 func New() *gorm.DB {
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbTimezone := os.Getenv("DB_TIMEZONE")
+	fmt.Println("database.New")
+	dbOnce.Do(func() {
+		fmt.Println("database.New.Once")
 
-	dsn := fmt.Sprintf(
-		"host=%s user=%s dbname=%s port=%s sslmode=disable TimeZone=%s",
-		dbHost, dbUser, dbName, dbPort, dbTimezone,
-	)
-	if dbPassword != "" {
-		dsn = fmt.Sprintf(
-			"%s password=%s",
-			dsn, dbPassword,
+		dbHost := os.Getenv("DB_HOST")
+		dbPort := os.Getenv("DB_PORT")
+		dbName := os.Getenv("DB_NAME")
+		dbUser := os.Getenv("DB_USER")
+		dbPassword := os.Getenv("DB_PASSWORD")
+		dbTimezone := os.Getenv("DB_TIMEZONE")
+
+		dsn := fmt.Sprintf(
+			"host=%s user=%s dbname=%s port=%s sslmode=disable TimeZone=%s",
+			dbHost, dbUser, dbName, dbPort, dbTimezone,
 		)
-	}
+		if dbPassword != "" {
+			dsn = fmt.Sprintf(
+				"%s password=%s",
+				dsn, dbPassword,
+			)
+		}
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err != nil {
+			panic("failed to connect to database: " + err.Error())
+		}
 
-	if err != nil {
-		panic("failed to connect to database: " + err.Error())
-	}
+		db.AutoMigrate(
+			&models.User{},
+			&models.UserToken{},
+			&models.Batch{},
+			&models.Major{},
+			&models.Classroom{},
+			&models.Student{},
+			&models.Attendance{},
+			&models.AttendanceStudent{},
+		)
 
-	db.AutoMigrate(
-		&models.User{},
-		&models.UserToken{},
-		&models.Batch{},
-		&models.Major{},
-		&models.Classroom{},
-		&models.Student{},
-		&models.Attendance{},
-		&models.AttendanceStudent{},
-	)
+		dbInstance = db
+	})
 
-	return db
+	return dbInstance
 }
